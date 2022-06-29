b0a71ed69eadc0d56d7d67aba6cb2f6445725409
shaoyuancc <shaoyuan.chewchia@letsbramble.com>
2022-06-29 22:29:45 +0800

    Testing script for vl53l0x set_address feature

--- examples/gyul53l0x.rs
+++ examples/gyul53l0x.rs
@@ -47,13 +47,21 @@ type DispType<'a> = Ssd1306<
 
 #[entry]
 fn main() -> ! {
-    if let (Some(dp), Some(_cp)) = (
+    if let (Some(dp), Some(cp)) = (
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
         // Set up the system clock. We want to run at 48MHz for this one.
         let rcc = dp.RCC.constrain();
         let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+
+        // Create a delay abstraction based on SysTick
+        let mut delay = cp.SYST.delay(&clocks);
+
+        // Set up the LED. On the Black Pill it's connected to pin PC13.
+        let gpioc = dp.GPIOC.split();
+        let mut led = gpioc.pc13.into_push_pull_output();
+        led.set_high();
 
         // Set up I2C - SCL is PB8 and SDA is PB9; they are set to Alternate Function 4
         let gpiob = dp.GPIOB.split();
@@ -76,9 +84,18 @@ fn main() -> ! {
         let gpioa = dp.GPIOA.split();
         let btn = gpioa.pa0.into_pull_up_input();
 
+        // Set up XShut pin
+        let mut xshut = gpiob.pb13.into_push_pull_output();
+        led.set_low();
+        xshut.set_low();
+        delay.delay_ms(200_u32);
+        led.set_high();
+        xshut.set_high();
+        delay.delay_ms(200_u32);
         // Set up TOF distance sensor
         // To create sensor with default configuration:
         let mut gyul53l0x = vl53l0x::VL53L0x::with_address(bus.acquire_i2c(), 0x29).expect("vl");
+
         gyul53l0x
             .set_measurement_timing_budget(200000)
             .expect("timbudg");
@@ -109,6 +126,7 @@ fn main() -> ! {
                 // On exiting state
                 match state {
                     Continuous => gyul53l0x.stop_continuous().expect("stop continuous"),
+                    AddressCycle => (), //gyul53l0x.set_address(0x29).expect("revert addr"),
                     _ => (),
                 };
 
@@ -121,6 +139,54 @@ fn main() -> ! {
                     WelcomeText => show_text(&WELCOME_TEXT, &mut disp),
                     Continuous => gyul53l0x.start_continuous(0).expect("start cont"),
                     Single => (),
+                    AddressCycle => {
+                        for i in 0_u8..255 {
+                            if i == 60 {
+                                continue; // Skip the I2C address of the OLED
+                            }
+                            match gyul53l0x.set_address(i) {
+                                Ok(()) => {
+                                    if !(i > 0x07 && i < 0x78) {
+                                        hprintln!(
+                                            "Failed cycle test! Invalid address was accepted"
+                                        )
+                                        .unwrap();
+                                    }
+                                }
+                                Err(_e) => {
+                                    if i > 0x07 && i < 0x78 {
+                                        hprintln!(
+                                            "Failed cycle test! Valid address {} did not work",
+                                            i
+                                        )
+                                        .unwrap();
+                                    } else {
+                                        hprintln!(
+                                            "correctly received error for invalid adddress {}",
+                                            i
+                                        )
+                                        .unwrap();
+                                        continue;
+                                    }
+                                }
+                            }
+                            match gyul53l0x.read_range_single_millimeters_blocking() {
+                                Ok(range) => {
+                                    let mut text: String<20> = String::from("Address: ");
+                                    text.push_str(&String::<3>::from(i)).unwrap();
+                                    text.push_str("\n").unwrap();
+                                    text.push_str(&String::<4>::from(range)).unwrap();
+                                    text.push_str("mm").unwrap();
+
+                                    show_text(&text, &mut disp);
+                                }
+                                Err(_e) => {
+                                    hprintln!("Error reading TOF sensor at address {}!", i).unwrap()
+                                }
+                            };
+                            delay.delay_ms(50_u32);
+                        }
+                    }
                     WhoAmI => {
                         let addr = gyul53l0x.who_am_i().expect("who am i");
                         let mut text: String<16> = String::from("Who Am I?\n");
@@ -139,9 +205,8 @@ fn main() -> ! {
                 Continuous => match gyul53l0x.read_range_continuous_millimeters_blocking() {
                     Ok(range) => {
                         let mut text: String<20> = String::from("Continuous\n");
-                        text.push_str(&String::<4>::from(range))
-                            .expect("string comp range");
-                        text.push_str("mm").expect("string comp unit");
+                        text.push_str(&String::<4>::from(range)).unwrap();
+                        text.push_str("mm").unwrap();
 
                         show_text(&text, &mut disp);
                     }
@@ -150,14 +215,14 @@ fn main() -> ! {
                 Single => match gyul53l0x.read_range_single_millimeters_blocking() {
                     Ok(range) => {
                         let mut text: String<20> = String::from("Single\n");
-                        text.push_str(&String::<4>::from(range))
-                            .expect("string composition - range");
-                        text.push_str("mm").expect("string composition - unit");
+                        text.push_str(&String::<4>::from(range)).unwrap();
+                        text.push_str("mm").unwrap();
 
                         show_text(&text, &mut disp);
                     }
                     Err(_e) => hprintln!("Error reading TOF sensor Single!").unwrap(),
                 },
+                AddressCycle => show_text("Done Cycling\nAddresses", &mut disp),
                 _ => (),
             };
         }
@@ -182,12 +247,13 @@ fn show_drawable(item: &impl Drawable<Color = BinaryColor>, disp: &mut DispType)
     item.draw(disp).unwrap();
     disp.flush().unwrap();
 }
+
 enum State {
     Image,
     WelcomeText,
     Continuous,
     Single,
-    // NonBlocking,
+    AddressCycle,
     WhoAmI,
     EndText,
 }
@@ -199,7 +265,8 @@ impl State {
             Image => WelcomeText,
             WelcomeText => Continuous,
             Continuous => Single,
-            Single => WhoAmI,
+            Single => AddressCycle,
+            AddressCycle => WhoAmI,
             WhoAmI => EndText,
             EndText => Image,
         }
